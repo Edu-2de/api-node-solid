b78a9bba1bb29eef35bad65de07da4b7cbb42d25
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-05-17 18:23:55 -0300

    feat: integrate Prisma with pg and set up user model
    Create a new pg datasource and user model in schema.prisma
    
    Enhanced environment variable validation to include DATABASE_URL
    
    Modified server.ts to use dynamic port from environment variables
    
    Updated tsconfig.json to suport path aliases for cleaner imports

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,17 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import fastify from "fastify";
+import { env } from "./env/index.js";
+import { PrismaClient } from "./generated/prisma/client.js";
 
+const connectionString = env.NODE_ENV;
+export const app = fastify();
 
-export const app = fastify()
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+prisma.user.create({
+    data: {
+        name: "Diego",
+        email: "email@gmail.com",
+    },
+});
