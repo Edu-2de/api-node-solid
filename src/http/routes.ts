3338e12122ae177763b5c3455034e7b2e08010c4
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-05-21 09:50:14 -0300

    feat: add profile controller and update app routes to include profile endpoint

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,11 @@
-import type { FastifyInstance } from "fastify";
-import { register } from "./controllers/register.js";
-import { authenticate } from "./controllers/authenticate.js";
+import type { FastifyInstance } from 'fastify';
+import { authenticate } from './controllers/authenticate.js';
+import { profile } from './controllers/profile.js';
+import { register } from './controllers/register.js';
 
 export async function appRoutes(app: FastifyInstance) {
-    app.post("/users", register);
-    app.post("/sessions", authenticate);
+  app.post('/users', register);
+  app.post('/sessions', authenticate);
+
+  app.get('/me', profile);
 }
