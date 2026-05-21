d05b32daf16ae97e0b4d5568a174621e98059e45
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-05-21 14:36:31 -0300

    feat: add nearby and search gym controllers; update gyms routes to include new endpoints

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,13 @@
 import { verifyJWT } from '@/http/middlewares/verify-jwt.js';
 import type { FastifyInstance } from 'fastify';
+import { create } from './create.js';
+import { nearby } from './nearby.js';
+import { search } from './search.js';
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT);
+
+  app.get('gyms/search', search);
+  app.get('gyms/nearby', nearby);
+  app.post('/gyms', create);
 }
